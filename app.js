a5a8618e8253d5774f1012bdd340d06a06f3ed1b
arnaldocarpi <arnaldo.carpi@icloud.com>
2022-09-12 17:33:35 -0300

    test ssl

--- app.js
+++ app.js
@@ -15,6 +15,35 @@ const Categoria = mongoose.model('categorias')
 const usuarios = require('./routes/usuario')
 const passport = require('passport')
 require('./config/auth')(passport)
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/carpi.serveblog.net/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/carpi.serveblog.net/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/carpi.serveblog.net/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+app.use((req, res) => {
+	res.send('Hello there !');
+});
+
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
 
 //configurações
     // configurar sessão
@@ -109,3 +138,8 @@ app.listen(PORT, () => console.log('Servidor WEB OK'))
 // infs
 // todo o app.use é a configuração de um middleware
 // /nome é referencia de link e nome/sem/barra é referencia de pasta
+
+
+// Dependencies
+
+// Certificate
